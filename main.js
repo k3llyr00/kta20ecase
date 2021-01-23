4c2aec17cafc51947e117d8d9ff38db1a9b7b6bf
Kaspar Suursalu <kasparsu@gmail.com>
2021-01-23 10:40:29 +0200

    add modules

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 console.log('hello node');
-alert("hello");
+const module = require('./module.js');
+module.hello();
+module.goodbye();
 
+import common from './module.js';
+common.hello();
+common.goodbye();
 let num = 1;
 num = "hello";
---
+++ module.js
@@ -0,0 +1,17 @@
+module.exports = {
+    hello: function() {
+        console.log("Hello module");
+    },
+    goodbye: function() {
+        console.log("goodbye module");
+    }
+}
+
+export default {
+    hello: function() {
+        console.log("Hello module");
+    },
+    goodbye: function() {
+        console.log("goodbye module");
+    }
+}
