443fe348b440e777749e3eed910bedcd91f6d651
Kaspar Suursalu <kasparsu@gmail.com>
2021-01-23 14:25:58 +0200

    bulma

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 console.log('hello node');
-const module = require('./module.js');
-module.hello();
-module.goodbye();
+const hello = require('./module.js').hello;
+hello();
 
 import common from './module.js';
-common.hello();
+common.hello()
 common.goodbye();
 let num = 1;
 num = "hello2";
+
+require('./main.ts');
