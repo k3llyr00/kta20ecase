8c3106b7ed93313cacb70dbad72985966f17104c
Kaspar Suursalu <kasparsu@gmail.com>
2021-02-18 09:26:00 +0200

    add demo.vue

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-console.log('hello node');
-const hello = require('./module.js').hello;
-hello();
+let el = document.getElementById('title');
+el.innerHTML = "Hello pure js";
 
-import common from './module.js';
-common.hello()
-common.goodbye();
-let num = 1;
-num = "hello2";
 
-require('./main.ts');
+import Vue from 'vue/dist/vue.common.js';
+
+import Demo from './demo.vue';
+Vue.component('demo', Demo);
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: "hello vue"
+    }
+});
