f7ffe2bab35a1bdc08a4afb558fbb941dc57cbe4
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-02-18 16:10:47 +0200

    add list example

--- demo.vue
+++ demo.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1 class="is-size-1">Hello {{name}}</h1>
+  <div>
+    <div class="columns">
+      <div class="column is-four-fifths">
+        <input class="input" type="text" v-model="newItem">
+      </div>
+      <div class="column is-one-fifths">
+        <button class="button" @click="addItem">Add item</button>
+      </div>
+    </div>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['name'],
+  props: [],
   data(){
     return {
-      message: "hello component"
+      newItem: '',
+      items: ['Sai', 'Leib', 'Õlu', 'Piim', 'Komm']
+    }
+  },
+  methods: {
+    addItem() {
+      if(this.newItem.trim() != ''){
+        this.items.push(this.newItem);
+      }
+      this.newItem = '';
     }
   } 
 }
