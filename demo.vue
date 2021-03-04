7a56df1160b0a77fde5689cddffc708597ee84a2
Kelly Roo <k3llyr00@gmail.com>
2021-03-04 16:26:45 +0200

    v-if and computed

--- demo.vue
+++ demo.vue
@@ -8,8 +8,29 @@
         <button class="button" @click="addItem">Add item</button>
       </div>
     </div>
+    <h3 class="is-size-3">{{ reversed }}</h3>
+    <h1 class="is-size-1">All items</h1>
     <ul>
-      <li v-for="item in items" :key="item">{{ item }}</li>
+      <li v-for="item in items" :key="item.key">
+        <input type="checkbox" v-model="item.isDone">
+        {{ item.name }}
+      </li>
+    </ul>
+
+    <h1 class="is-size-1">Done items</h1>
+    <ul>
+      <li v-for="item in doneItems" :key="item.key">
+        <input type="checkbox" v-model="item.isDone">
+        {{ item.name }}
+      </li>
+    </ul>
+
+    <h1 class="is-size-1">Not Done items</h1>
+    <ul>
+      <li v-for="item in notDoneItems" :key="item.key">
+        <input type="checkbox" v-model="item.isDone">
+        {{ item.name }}
+      </li>
     </ul>
   </div>
 </template>
@@ -20,20 +41,43 @@ export default {
   data(){
     return {
       newItem: '',
-      items: ['Sai', 'Leib', 'Õlu', 'Piim', 'Komm']
+      items: [
+        {name:'Sai', isDone: false, key: 0},
+        {name:'Leib', isDone: true, key: 1},
+        {name:'Õlu', isDone: false, key: 2},
+        {name:'Piim', isDone: true, key: 3},
+        {name:'Komm', isDone: false, key: 4},
+      ]
     }
   },
   methods: {
     addItem() {
       if(this.newItem.trim() != ''){
-        this.items.push(this.newItem);
+        this.items.push({name:this.newItem, isDone: false, key: this.items.length});
       }
       this.newItem = '';
     }
-  } 
+  },
+  computed: {
+    // "hello" ['h', 'e', 'l', 'l', 'o'] 'olleh'
+    reversed(){
+      return this.newItem.split('').reverse().join('');
+    },
+    doneItems(){
+      // return this.items.filter(function (item) {
+      //     return item.isDone;
+      // });
+      // return this.items.filter(item => {
+      //   return item.isDone;
+      // });
+      return this.items.filter(item => item.isDone);
+    },
+    notDoneItems(){
+      return this.items.filter(item => !item.isDone);
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
